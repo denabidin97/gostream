74cf1749093e22772e88b1d8c10afffb0e01f022
denabidin97 <denabidin97@gmail.com>
2025-10-16 11:37:33 +0700

    channeltv.js

--- channeltv.js
+++ channeltv.js
@@ -2,120 +2,121 @@
   {
     "id": "sctv",
     "name": "SCTV",
-    "logo": "https://upload.wikimedia.org/wikipedia/commons/2/2a/Logo_SCTV_2013.png",
+    "logo": "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cc/SCTV_Logo.svg/960px-SCTV_Logo.svg.png",
     "url": "https://librani.govoet.my.id/?id=sctv",
     "status": "Online"
   },
   {
     "id": "indosiar",
     "name": "Indosiar",
-    "logo": "https://upload.wikimedia.org/wikipedia/id/5/57/Logo_Indosiar.png",
+    "logo": "https://upload.wikimedia.org/wikipedia/commons/thumb/3/39/INDOSIAR_Logo.png/960px-INDOSIAR_Logo.png",
     "url": "https://librani.govoet.my.id/?id=indosiar",
     "status": "Online"
   },
   {
     "id": "moji",
     "name": "Moji",
-    "logo": "https://upload.wikimedia.org/wikipedia/commons/0/00/Logo_Moji_2022.png",
+    "logo": "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c9/Moji_blue.svg/250px-Moji_blue.svg.png",
     "url": "https://librani.govoet.my.id/?id=moji",
     "status": "Online"
   },
   {
     "id": "tvrisports",
     "name": "TVRI Sports",
-    "logo": "https://upload.wikimedia.org/wikipedia/commons/4/43/TVRI_Sport_Logo.png",
+    "logo": "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/TVRI_Sport_2022.svg/250px-TVRI_Sport_2022.svg.png",
     "url": "https://librani.govoet.my.id/?id=tvrisports",
     "status": "Online"
   },
   {
     "id": "tvriworld",
     "name": "TVRI World",
-    "logo": "https://upload.wikimedia.org/wikipedia/commons/f/f4/TVRI_World_Logo.png",
+    "logo": "https://upload.wikimedia.org/wikipedia/commons/thumb/6/64/TVRI-World.svg/250px-TVRI-World.svg.png",
     "url": "https://librani.govoet.my.id/?id=tvriworld",
     "status": "Online"
   },
   {
     "id": "metrotv",
     "name": "Metro TV",
-    "logo": "https://upload.wikimedia.org/wikipedia/commons/f/fb/Logo_Metro_TV_2021.svg",
+    "logo": "https://upload.wikimedia.org/wikipedia/id/thumb/c/c1/LogoMetroTVsince2010.svg/250px-LogoMetroTVsince2010.svg.png",
     "url": "https://librani.govoet.my.id/?id=metrotv",
     "status": "Online"
   },
   {
     "id": "tvone",
     "name": "TV One",
-    "logo": "https://upload.wikimedia.org/wikipedia/commons/8/8f/TVOne_Logo_2021.svg",
+    "logo": "https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/TvOne_2023.svg/960px-TvOne_2023.svg.png",
     "url": "https://librani.govoet.my.id/?id=tvone",
     "status": "Online"
   },
   {
     "id": "kompas",
     "name": "Kompas TV",
-    "logo": "https://upload.wikimedia.org/wikipedia/commons/2/2d/Kompas_TV_logo_2021.svg",
+    "logo": "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/KOMPAS_TV_%282017%29.svg/250px-KOMPAS_TV_%282017%29.svg.png",
     "url": "https://librani.govoet.my.id/?id=kompas",
     "status": "Online"
   },
   {
     "id": "btv",
     "name": "BTV",
-    "logo": "https://upload.wikimedia.org/wikipedia/commons/8/83/BeritaSatuTV_logo.svg",
+    "logo": "https://upload.wikimedia.org/wikipedia/commons/thumb/9/94/BTV_%28Indonesia%29_logo.svg/244px-BTV_%28Indonesia%29_logo.svg.png",
     "url": "https://librani.govoet.my.id/?id=btv",
     "status": "Online"
   },
   {
     "id": "rtv",
     "name": "RTV",
-    "logo": "https://upload.wikimedia.org/wikipedia/commons/2/2b/RTV_Logo_2014.png",
+    "logo": "https://upload.wikimedia.org/wikipedia/id/thumb/5/5a/Rajawali_Televisi.svg/300px-Rajawali_Televisi.svg.png?20180624132916",
     "url": "https://librani.govoet.my.id/?id=rtv",
     "status": "Online"
   },
   {
     "id": "antv",
     "name": "ANTV",
-    "logo": "https://upload.wikimedia.org/wikipedia/commons/0/0f/ANTV_logo_2017.svg",
+    "logo": "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2e/Antv_logo.svg/480px-Antv_logo.svg.png",
     "url": "https://librani.govoet.my.id/?id=antv",
     "status": "Online"
   },
   {
     "id": "rcti",
     "name": "RCTI",
-    "logo": "https://upload.wikimedia.org/wikipedia/commons/0/02/RCTI_Logo.svg",
+    "logo": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/RCTI_logo_2015.svg/300px-RCTI_logo_2015.svg.png",
     "url": "https://librani.govoet.my.id/?id=rcti",
     "status": "Online"
   },
   {
     "id": "mnctv",
     "name": "MNCTV",
-    "logo": "https://upload.wikimedia.org/wikipedia/commons/9/99/Logo_MNCTV_2020.svg",
+    "logo": "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/MNC_Media_2015.png/250px-MNC_Media_2015.png",
     "url": "https://librani.govoet.my.id/?id=mnctv",
     "status": "Online"
   },
   {
     "id": "gtv",
     "name": "GTV",
-    "logo": "https://upload.wikimedia.org/wikipedia/commons/d/d3/GTV_logo_2017.svg",
+    "logo": "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9c/GTV_%282017%29.svg/250px-GTV_%282017%29.svg.png",
     "url": "https://librani.govoet.my.id/?id=gtv",
     "status": "Online"
   },
   {
     "id": "net",
     "name": "NET TV",
-    "logo": "https://upload.wikimedia.org/wikipedia/commons/4/4f/NET._Mediatelevisi_Logo_2013.svg",
+    "logo": "https://asset-2.tribunnews.com/tribunnews/foto/bank/images/logo-net-tv.jpg",
     "url": "https://librani.govoet.my.id/?id=net",
     "status": "Online"
   },
   {
     "id": "transtv",
     "name": "Trans TV",
-    "logo": "https://upload.wikimedia.org/wikipedia/commons/4/48/Trans_TV_logo_2013.svg",
+    "logo": "https://static.wikia.nocookie.net/sketsa/images/4/44/Logotranstv.png",
     "url": "https://librani.govoet.my.id/?id=transtv",
     "status": "Online"
   },
   {
     "id": "trans7",
     "name": "Trans7",
-    "logo": "https://upload.wikimedia.org/wikipedia/commons/0/02/Trans7_logo_2013.svg",
+    "logo": "https://upload.wikimedia.org/wikipedia/commons/thumb/9/96/Logo_Trans7.png/960px-Logo_Trans7.png",
     "url": "https://librani.govoet.my.id/?id=trans7",
     "status": "Online"
   }
 ]
+
